85f8e27be28c59a16bbb292b333d7072b6f9ad35
Matthias Hörmann <hoermann@saltation.de>
2022-01-26 22:13:26 +0100

    API for project memberships and roles

--- src/api/project_memberships.rs
+++ src/api/project_memberships.rs
@@ -12,26 +12,56 @@ use derive_builder::Builder;
 use http::Method;
 use std::borrow::Cow;
 
-use crate::api::Endpoint;
+use crate::api::groups::GroupEssentials;
+use crate::api::projects::ProjectEssentials;
+use crate::api::roles::RoleEssentials;
+use crate::api::users::UserEssentials;
+use crate::api::{Endpoint, Pageable, ReturnsJsonResponse};
 use serde::Serialize;
+
+/// a type for project memberships to use as an API return type
+///
+/// alternatively you can use your own type limited to the fields you need
+#[derive(Debug, PartialEq, Eq, Serialize, serde::Deserialize)]
+pub struct ProjectMembership {
+    /// numeric id
+    pub id: u64,
+    /// the project
+    pub project: ProjectEssentials,
+    /// the user (project member), optional because alternatively we could have a group
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub user: Option<UserEssentials>,
+    /// the group (project member), optional because alternatively we could have a user
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub group: Option<GroupEssentials>,
+    /// the roles the user or group has in the project
+    pub roles: Vec<RoleEssentials>,
+}
 
 /// The endpoint for all memberships in a Redmine project
 #[derive(Debug, Builder)]
 #[builder(setter(strip_option))]
-pub struct ProjectMemberships<'a> {
+pub struct ListProjectMemberships<'a> {
     /// project id or name as it appears in the URL
     #[builder(setter(into))]
     project_id_or_name: Cow<'a, str>,
 }
 
-impl<'a> ProjectMemberships<'a> {
-    /// Create a builder for the endpoint.
-    pub fn builder() -> ProjectMembershipsBuilder<'a> {
-        ProjectMembershipsBuilder::default()
+impl<'a> ReturnsJsonResponse for ListProjectMemberships<'a> {}
+impl<'a> Pageable for ListProjectMemberships<'a> {
+    fn response_wrapper_key(&self) -> String {
+        "memberships".to_string()
     }
 }
 
-impl<'a> Endpoint for ProjectMemberships<'a> {
+impl<'a> ListProjectMemberships<'a> {
+    /// Create a builder for the endpoint.
+    pub fn builder() -> ListProjectMembershipsBuilder<'a> {
+        ListProjectMembershipsBuilder::default()
+    }
+}
+
+impl<'a> Endpoint for ListProjectMemberships<'a> {
     fn method(&self) -> Method {
         Method::GET
     }
@@ -44,19 +74,21 @@ impl<'a> Endpoint for ProjectMemberships<'a> {
 /// The endpoint for a specific Redmine project membership
 #[derive(Debug, Builder)]
 #[builder(setter(strip_option))]
-pub struct ProjectMembership {
+pub struct GetProjectMembership {
     /// id of the project membership to retrieve
     id: u64,
 }
 
-impl<'a> ProjectMembership {
+impl ReturnsJsonResponse for GetProjectMembership {}
+
+impl<'a> GetProjectMembership {
     /// Create a builder for the endpoint.
-    pub fn builder() -> ProjectMembershipBuilder {
-        ProjectMembershipBuilder::default()
+    pub fn builder() -> GetProjectMembershipBuilder {
+        GetProjectMembershipBuilder::default()
     }
 }
 
-impl<'a> Endpoint for ProjectMembership {
+impl<'a> Endpoint for GetProjectMembership {
     fn method(&self) -> Method {
         Method::GET
     }
@@ -67,7 +99,8 @@ impl<'a> Endpoint for ProjectMembership {
 }
 
 /// The endpoint to create a Redmine project membership (add a user or group to a project)
-#[derive(Debug, Builder, Serialize)]
+#[serde_with::skip_serializing_none]
+#[derive(Debug, Clone, Builder, Serialize)]
 #[builder(setter(strip_option))]
 pub struct CreateProjectMembership<'a> {
     /// project id or name as it appears in the URL
@@ -79,6 +112,8 @@ pub struct CreateProjectMembership<'a> {
     /// roles for the user to add to the project
     role_ids: Vec<u64>,
 }
+
+impl<'a> ReturnsJsonResponse for CreateProjectMembership<'a> {}
 
 impl<'a> CreateProjectMembership<'a> {
     /// Create a builder for the endpoint.
@@ -97,12 +132,18 @@ impl<'a> Endpoint for CreateProjectMembership<'a> {
     }
 
     fn body(&self) -> Result<Option<(&'static str, Vec<u8>)>, crate::Error> {
-        Ok(Some(("application/json", serde_json::to_vec(self)?)))
+        Ok(Some((
+            "application/json",
+            serde_json::to_vec(&MembershipWrapper::<CreateProjectMembership> {
+                membership: (*self).to_owned(),
+            })?,
+        )))
     }
 }
 
 /// The endpoint to update an existing Redmine project membership (change roles)
-#[derive(Debug, Builder, Serialize)]
+#[serde_with::skip_serializing_none]
+#[derive(Debug, Clone, Builder, Serialize)]
 #[builder(setter(strip_option))]
 pub struct UpdateProjectMembership {
     /// id of the project membership to update
@@ -129,7 +170,12 @@ impl<'a> Endpoint for UpdateProjectMembership {
     }
 
     fn body(&self) -> Result<Option<(&'static str, Vec<u8>)>, crate::Error> {
-        Ok(Some(("application/json", serde_json::to_vec(self)?)))
+        Ok(Some((
+            "application/json",
+            serde_json::to_vec(&MembershipWrapper::<UpdateProjectMembership> {
+                membership: (*self).to_owned(),
+            })?,
+        )))
     }
 }
 
@@ -155,5 +201,139 @@ impl<'a> Endpoint for DeleteProjectMembership {
 
     fn endpoint(&self) -> Cow<'static, str> {
         format!("memberships/{}.json", &self.id).into()
+    }
+}
+
+/// helper struct for outer layers with a memberships field holding the inner data
+#[derive(Debug, PartialEq, Eq, Serialize, serde::Deserialize)]
+pub struct MembershipsWrapper<T> {
+    /// to parse JSON with memberships key
+    pub memberships: Vec<T>,
+}
+
+/// A lot of APIs in Redmine wrap their data in an extra layer, this is a
+/// helper struct for outer layers with a membership field holding the inner data
+#[derive(Debug, PartialEq, Eq, Serialize, serde::Deserialize)]
+pub struct MembershipWrapper<T> {
+    /// to parse JSON with membership key
+    pub membership: T,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::api::test_helpers::with_project;
+    use pretty_assertions::assert_eq;
+    use std::error::Error;
+    use tracing_test::traced_test;
+
+    #[traced_test]
+    #[test]
+    fn test_list_project_memberships_no_pagination() -> Result<(), Box<dyn Error>> {
+        dotenv::dotenv()?;
+        let redmine = crate::api::Redmine::from_env()?;
+        let endpoint = ListProjectMemberships::builder()
+            .project_id_or_name("sandbox")
+            .build()?;
+        redmine.json_response_body::<_, MembershipsWrapper<ProjectMembership>>(&endpoint)?;
+        Ok(())
+    }
+
+    #[traced_test]
+    #[test]
+    fn test_list_project_memberships_first_page() -> Result<(), Box<dyn Error>> {
+        dotenv::dotenv()?;
+        let redmine = crate::api::Redmine::from_env()?;
+        let endpoint = ListProjectMemberships::builder()
+            .project_id_or_name("sandbox")
+            .build()?;
+        redmine.json_response_body_page::<_, ProjectMembership>(&endpoint, 0, 25)?;
+        Ok(())
+    }
+
+    #[traced_test]
+    #[test]
+    fn test_list_project_memberships_all_pages() -> Result<(), Box<dyn Error>> {
+        dotenv::dotenv()?;
+        let redmine = crate::api::Redmine::from_env()?;
+        let endpoint = ListProjectMemberships::builder()
+            .project_id_or_name("sandbox")
+            .build()?;
+        redmine.json_response_body_all_pages::<_, ProjectMembership>(&endpoint)?;
+        Ok(())
+    }
+
+    #[traced_test]
+    #[test]
+    fn test_get_project_membership() -> Result<(), Box<dyn Error>> {
+        dotenv::dotenv()?;
+        let redmine = crate::api::Redmine::from_env()?;
+        let endpoint = GetProjectMembership::builder().id(238).build()?;
+        redmine.json_response_body::<_, MembershipWrapper<ProjectMembership>>(&endpoint)?;
+        Ok(())
+    }
+
+    #[function_name::named]
+    #[traced_test]
+    #[test]
+    fn test_create_project_membership() -> Result<(), Box<dyn Error>> {
+        let name = format!("unittest_{}", function_name!());
+        with_project(&name, |redmine, project_id, _| {
+            let create_endpoint = super::CreateProjectMembership::builder()
+                .project_id_or_name(project_id.to_string())
+                .user_id(1)
+                .role_ids(vec![8])
+                .build()?;
+            redmine
+                .json_response_body::<_, MembershipWrapper<ProjectMembership>>(&create_endpoint)?;
+            Ok(())
+        })?;
+        Ok(())
+    }
+
+    #[function_name::named]
+    #[traced_test]
+    #[test]
+    fn test_update_project_membership() -> Result<(), Box<dyn Error>> {
+        let name = format!("unittest_{}", function_name!());
+        with_project(&name, |redmine, project_id, _name| {
+            let create_endpoint = super::CreateProjectMembership::builder()
+                .project_id_or_name(project_id.to_string())
+                .user_id(1)
+                .role_ids(vec![8])
+                .build()?;
+            let MembershipWrapper { membership } = redmine
+                .json_response_body::<_, MembershipWrapper<ProjectMembership>>(&create_endpoint)?;
+            let update_endpoint = super::UpdateProjectMembership::builder()
+                .id(membership.id)
+                .role_ids(vec![9])
+                .build()?;
+            redmine.ignore_response_body::<_>(&update_endpoint)?;
+            Ok(())
+        })?;
+        Ok(())
+    }
+
+    /// this tests if any of the results contain a field we are not deserializing
+    ///
+    /// this will only catch fields we missed if they are part of the response but
+    /// it is better than nothing
+    #[traced_test]
+    #[test]
+    fn test_completeness_project_membership_type() -> Result<(), Box<dyn Error>> {
+        dotenv::dotenv()?;
+        let redmine = crate::api::Redmine::from_env()?;
+        let endpoint = ListProjectMemberships::builder()
+            .project_id_or_name("sandbox")
+            .build()?;
+        let MembershipsWrapper {
+            memberships: values,
+        } = redmine.json_response_body::<_, MembershipsWrapper<serde_json::Value>>(&endpoint)?;
+        for value in values {
+            let o: ProjectMembership = serde_json::from_value(value.clone())?;
+            let reserialized = serde_json::to_value(o)?;
+            assert_eq!(value, reserialized);
+        }
+        Ok(())
     }
 }
--- src/api/roles.rs
+++ src/api/roles.rs
@@ -9,31 +9,98 @@ use derive_builder::Builder;
 use http::Method;
 use std::borrow::Cow;
 
-use crate::api::Endpoint;
+use crate::api::{Endpoint, ReturnsJsonResponse};
 
 /// a minimal type for Redmine roles used in lists of roles included in
-/// other Redmine objects (e.g. custom fields)
+/// other Redmine objects (e.g. custom fields) and also in the global ListRoles
+/// endpoint (unlike most other Redmine API objects)
 #[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct RoleEssentials {
     /// numeric id
     id: u64,
     /// display name
     name: String,
+    /// true if this role is inherited from a parent project, used e.g. in project memberships
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    inherited: Option<bool>,
+}
+
+/// determines which issues are visible to users/group with a role
+#[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub enum IssuesVisibility {
+    /// a user/group with the role can see all issues (in visible projects)
+    #[serde(rename = "all")]
+    All,
+    /// a user/group with the role can see all non-private issues (in visible projects)
+    #[serde(rename = "default")]
+    AllNonPrivate,
+    /// a user/group with the role can see only issues created by or assigned to them
+    #[serde(rename = "own")]
+    Own,
+}
+
+/// determines which time entries are visible to users/group with a role
+#[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub enum TimeEntriesVisibility {
+    /// a user/group with the role can see all time entries (in visible projects)
+    #[serde(rename = "all")]
+    All,
+    /// a user/group with the role can see only time entries created by them
+    #[serde(rename = "own")]
+    Own,
+}
+
+/// determines which users are visible to users/group with a role
+#[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub enum UsersVisibility {
+    /// a user/group with the role can see all active users
+    #[serde(rename = "all")]
+    All,
+    /// a user/group with the role can only see users which are members of the project
+    #[serde(rename = "members_of_visible_projects")]
+    MembersOfVisibleProjects,
+}
+
+/// a type for roles to use as an API return type
+///
+/// alternatively you can use your own type limited to the fields you need
+#[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct Role {
+    /// numeric id
+    pub id: u64,
+    /// display name
+    pub name: String,
+    /// if this is true users/groups with this role can be assignees for issues
+    pub assignable: bool,
+    /// the issues that can be seen by users/groups with this role
+    pub issues_visibility: IssuesVisibility,
+    /// the time entries that can be seen by users/groups with this role
+    pub time_entries_visibility: TimeEntriesVisibility,
+    /// the users that can be seen by users/groups with this role
+    pub users_visibility: UsersVisibility,
+    /// list of permissions, this can contain core Redmine permissions
+    /// and those provided by plugins
+    pub permissions: Vec<String>,
 }
 
 /// The endpoint for all roles
+///
+/// unlike most other Redmine objects this only returns a RoleEssentials like
+/// minimal object
 #[derive(Debug, Builder)]
 #[builder(setter(strip_option))]
-pub struct Roles {}
+pub struct ListRoles {}
 
-impl Roles {
+impl ReturnsJsonResponse for ListRoles {}
+
+impl ListRoles {
     /// Create a builder for the endpoint.
-    pub fn builder() -> RolesBuilder {
-        RolesBuilder::default()
+    pub fn builder() -> ListRolesBuilder {
+        ListRolesBuilder::default()
     }
 }
 
-impl<'a> Endpoint for Roles {
+impl<'a> Endpoint for ListRoles {
     fn method(&self) -> Method {
         Method::GET
     }
@@ -46,24 +113,91 @@ impl<'a> Endpoint for Roles {
 /// The endpoint for a specific role
 #[derive(Debug, Builder)]
 #[builder(setter(strip_option))]
-pub struct Role {
+pub struct GetRole {
     /// the id of the role to retrieve
     id: u64,
 }
 
-impl Role {
+impl ReturnsJsonResponse for GetRole {}
+
+impl GetRole {
     /// Create a builder for the endpoint.
-    pub fn builder() -> RoleBuilder {
-        RoleBuilder::default()
+    pub fn builder() -> GetRoleBuilder {
+        GetRoleBuilder::default()
     }
 }
 
-impl<'a> Endpoint for Role {
+impl<'a> Endpoint for GetRole {
     fn method(&self) -> Method {
         Method::GET
     }
 
     fn endpoint(&self) -> Cow<'static, str> {
         format!("roles/{}.json", self.id).into()
+    }
+}
+
+/// helper struct for outer layers with a roles field holding the inner data
+#[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct RolesWrapper<T> {
+    /// to parse JSON with roles key
+    pub roles: Vec<T>,
+}
+
+/// A lot of APIs in Redmine wrap their data in an extra layer, this is a
+/// helper struct for outer layers with a role field holding the inner data
+#[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct RoleWrapper<T> {
+    /// to parse JSON with role key
+    pub role: T,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use std::error::Error;
+    use tracing_test::traced_test;
+
+    #[traced_test]
+    #[test]
+    fn test_list_roles_no_pagination() -> Result<(), Box<dyn Error>> {
+        dotenv::dotenv()?;
+        let redmine = crate::api::Redmine::from_env()?;
+        let endpoint = ListRoles::builder().build()?;
+        redmine.json_response_body::<_, RolesWrapper<RoleEssentials>>(&endpoint)?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_role() -> Result<(), Box<dyn Error>> {
+        dotenv::dotenv()?;
+        let redmine = crate::api::Redmine::from_env()?;
+        let endpoint = GetRole::builder().id(8).build()?;
+        redmine.json_response_body::<_, RoleWrapper<Role>>(&endpoint)?;
+        Ok(())
+    }
+
+    /// this tests if any of the results contain a field we are not deserializing
+    ///
+    /// this will only catch fields we missed if they are part of the response but
+    /// it is better than nothing
+    #[traced_test]
+    #[test]
+    fn test_completeness_role_type() -> Result<(), Box<dyn Error>> {
+        dotenv::dotenv()?;
+        let redmine = crate::api::Redmine::from_env()?;
+        let list_endpoint = ListRoles::builder().build()?;
+        let RolesWrapper { roles } =
+            redmine.json_response_body::<_, RolesWrapper<RoleEssentials>>(&list_endpoint)?;
+        for role in roles {
+            let endpoint = GetRole::builder().id(role.id).build()?;
+            let RoleWrapper { role: value } =
+                redmine.json_response_body::<_, RoleWrapper<serde_json::Value>>(&endpoint)?;
+            let o: Role = serde_json::from_value(value.clone())?;
+            let reserialized = serde_json::to_value(o)?;
+            assert_eq!(value, reserialized);
+        }
+        Ok(())
     }
 }
