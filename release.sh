f599c603e9c308a65b21177bcbe41a5c826fd173
Matthias Hörmann <matthias.hoermann@saltation.com>
2025-11-06 18:46:41 +0100

    build(release): Replace hand-written line-break algorithm with rumdl
    Turns out rumdl can wrap lines but that fix is disabled by default.
    
    Ran_pre-commit_check: markdown:::rumdl
    Ran_pre-commit_check: shell:::shellcheck
    Ran_pre-commit_check: shell:::shfmt
    Ran_pre-commit_check: toml:::taplo
    Ran_pre-commit_check: rust:::cargo-check
    Ran_pre-commit_check: rust:::cargo-deny-advisories
    Ran_pre-commit_check: rust:::cargo-deny-licenses
    Ran_pre-commit_check: rust:::cargo-deny-sources
    Ran_pre-commit_check: rust:::cargo-sort
    Ran_pre-commit_check: rust:::changelog-md-includes-version
    Ran_pre-commit_check: rust:::clippy
    Ran_pre-commit_check: rust:::crate-version-not-in-cargo-lock
    Ran_pre-commit_check: rust:::doc
    Ran_pre-commit_check: rust:::formatting
    Ran_pre-commit_check: rust:::lib:::semver-violations
    Ran_pre-commit_check: rust:::test
    Ran_pre-commit_check: rust:::unused-dependencies
    Ran_pre-commit_check: common:::whitespace
    Ran_pre-commit_check: common:::non-ascii-filenames
    Ran_pre-commit_check: common:::typos

--- release.sh
+++ release.sh
@@ -15,44 +15,7 @@ version="$(cargo get package.version)"
 
 git cliff --prepend CHANGELOG.md -u -t "redmine_api_${version}"
 
-# break list entries (one is generated per commit) with long lines
-# over 80 characters at the last space before character 80 and indent
-# them so they are still part of the list entry
-list_item_re='^- '
-while read -r line; do
-  if [[ "${line}" =~ ${list_item_re} ]]; then
-    if [[ ${#line} -gt 80 ]]; then
-      next="${line:0:80}"
-      next_without_suffix="${next% *}"
-      if [[ "${next}" == "${next_without_suffix}" ]]; then
-        # can't break this line, no spaces
-        echo "${line}"
-        continue
-      fi
-      echo "${next_without_suffix}"
-      next_len=${#next_without_suffix}
-      next_len=$((next_len + 1))
-      rest="${line:${next_len}}"
-      while [[ -n "${rest}" ]]; do
-        next="${rest:0:78}"
-        next_without_suffix="${next% *}"
-        if [[ "${next}" == "${next_without_suffix}" ]]; then
-          # can't break the rest, no spaces
-          echo "  ${rest}"
-          continue 2
-        fi
-        echo "  ${next_without_suffix}"
-        next_len=${#next_without_suffix}
-        next_len=$((next_len + 1))
-        rest="${rest:${next_len}}"
-      done
-    else
-      echo "${line}"
-    fi
-  else
-    echo "${line}"
-  fi
-done <CHANGELOG.md | sponge CHANGELOG.md
+rumdl fmt --fix CHANGELOG.md
 
 cargo build
 
